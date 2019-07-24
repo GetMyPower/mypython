74efbf8673b054c4f24710c7b76da388c00d0c7e
GetMyPower <38427686+GetMyPower@users.noreply.github.com>
2019-07-24 21:49:46 +0800

    Update 9-3.py

--- TensorFlow深度学习应用实践_源代码/09/9-3.py
+++ TensorFlow深度学习应用实践_源代码/09/9-3.py
@@ -12,7 +12,7 @@ def make_matrix(m,n,fill=0.0):
     return mat
 
 def sigmoid(x):
-return 1.0 / (1.0 + math.exp(-x))
+    return 1.0 / (1.0 + math.exp(-x))
 
 def sigmod_derivate(x):
     return x * (1 - x)
