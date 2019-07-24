5afd6f605e7b3b370c968281bd1f129344d1e677
GetMyPower <38427686+GetMyPower@users.noreply.github.com>
2019-07-24 16:13:18 +0800

    Update 8-3.py

--- TensorFlow深度学习应用实践_源代码/08/8-3.py
+++ TensorFlow深度学习应用实践_源代码/08/8-3.py
@@ -13,27 +13,27 @@ inputX = np.random.rand(3000,1)
 noise = np.random.normal(0, 0.05, inputX.shape)
 outputY = inputX * 4 + 1 + noise
 
-#这里是第一层
+# 1st layer
 weight1 = tf.Variable(np.random.rand(inputX.shape[1],4))
 bias1 = tf.Variable(np.random.rand(inputX.shape[1],4))
-x1 = tf.placeholder(tf.float64, [None, 1])
-y1_ = tf.matmul(x1, weight1) + bias1
+x1 = tf.placeholder(tf.float64, [None, 1])  # 占位符1，自变量，在run时输入
+y1_ = tf.matmul(x1, weight1) + bias1   # 建立的一元线性回归模型
 
-y = tf.placeholder(tf.float64, [None, 1])
-loss = tf.reduce_mean(tf.reduce_sum(tf.square((y2_ - y)), reduction_indices=[1]))
+y = tf.placeholder(tf.float64, [None, 1])  # 占位符2，因变量，训练数据，在run时输入
+loss = tf.reduce_mean(tf.reduce_sum(tf.square((y1_ - y)), reduction_indices=[1]))  # 各列差的平方和的平均值
 train = tf.train.GradientDescentOptimizer(0.25).minimize(loss)  # 选择梯度下降法
 
-init = tf.initialize_all_variables()
-sess = tf.Session()
-sess.run(init)
+init = tf.initialize_all_variables()  # 初始化变量
+sess = tf.Session()  # 新建会话
+sess.run(init)  # 开始执行
 
 for i in range(1000):
-    sess.run(train, feed_dict={x1: inputX, y: outputY})
+    sess.run(train, feed_dict={x1: inputX, y: outputY})  # 按照占位符输入训练数据，按照选定的算法训练
 
 print(weight1.eval(sess))
 print("---------------------")
 print(bias1.eval(sess))
 print("------------------结果是------------------")
 
-x_data = np.matrix([[1.],[2.],[3.]])
-print(sess.run(y1_,feed_dict={x1: x_data}))
+x_data = np.matrix([[1.],[2.],[3.]])    # 网络训练完毕，输入待计算的自变量
+print(sess.run(y1_,feed_dict={x1: x_data}))    # 根据已经生成的网络预测新值
