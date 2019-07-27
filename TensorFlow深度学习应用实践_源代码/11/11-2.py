3e00dbf1fd4186551ccfb82fc199601f1e5877b4
GetMyPower <38427686+GetMyPower@users.noreply.github.com>
2019-07-27 09:33:05 +0800

    Update 11-2.py

--- TensorFlow深度学习应用实践_源代码/11/11-2.py
+++ TensorFlow深度学习应用实践_源代码/11/11-2.py
@@ -9,16 +9,16 @@ import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
 
-x_data = np.random.randn(100)#.astype(np.float32)
-y_data = x_data * 0.3 + 0.1
+x_data = np.random.rand(100)
+y_data = 0.3 * x_data + 0.15
 
 weight = tf.Variable(0.5)
-bias = tf.Variable(0.0)
+bias = tf.Variable(0.5)
 x_ = tf.placeholder(tf.float32)
 y_ = tf.placeholder(tf.float32)
 y_model = weight * x_ + bias
 
-loss = tf.pow((y_model - y_),2)
+loss = tf.pow(y_model - y_, 2)
 train_op = tf.train.GradientDescentOptimizer(0.01).minimize(loss)
 
 sess = tf.Session()
@@ -28,7 +28,7 @@ sess.run(init)
 for _ in range(10):
     for (x,y) in zip(x_data,y_data):
         sess.run(train_op,feed_dict={x_:x,y_:y})
-    print("weighe: " ,weight.eval(sess)," | bias: ",bias.eval(sess))
+    print("weight = %.4f | bias = %.4f" % (weight.eval(sess), bias.eval(sess)))
 
 plt.plot(x_data, y_data, 'ro', label='Original data')
 plt.plot(x_data, sess.run(weight) * (x_data) + sess.run(bias), label='Fitted line')
