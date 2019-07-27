f8f5a4cb44bb527ddc672892d6b78ef159d105ac
GetMyPower <38427686+GetMyPower@users.noreply.github.com>
2019-07-27 20:56:23 +0800

    Update 11-12.py

--- TensorFlow深度学习应用实践_源代码/11/11-12.py
+++ TensorFlow深度学习应用实践_源代码/11/11-12.py
@@ -1,5 +1,5 @@
 """
-loss太大，神经网络不收敛，无法计算
+loss太大，神经网络不收敛，偶尔无法计算。注意学习速率的设定
 """
 
 import tensorflow as tf
@@ -8,29 +8,35 @@ import numpy as np
 houses = 100
 features = 2
 
-#设计的模型为 2 * x1 + 3 * x2
-x_data = np.zeros([houses,2])
-for house in range(houses):
-    x_data[house,0] = np.round(np.random.uniform(50., 150.))
-    x_data[house,1] = np.round(np.random.uniform(3., 7.))
-weights = np.array([[2.],[3.]])
-y_data = np.dot(x_data,weights)
+x_data = np.zeros([houses, 2])
+for i in range(houses):
+    x_data[i, 0] = np.round(np.random.uniform(50., 150.))
+    x_data[i, 1] = np.round(np.random.uniform(3., 7.))
+weights = np.array([[2.], [3.]])
+y_data = np.dot(x_data, weights)
 
-x_data_ = tf.placeholder(tf.float32,[None,2])
-y_data_ = tf.placeholder(tf.float32,[None,1])
-weights_ = tf.Variable(np.ones([2,1]),dtype=tf.float32)
-y_model = tf.matmul(x_data_,weights_)
+x_data_ = tf.placeholder(tf.float32, [None, 2])
+y_data_ = tf.placeholder(tf.float32, [None, 1])
+weights_ = tf.Variable(np.ones([2, 1]), dtype=tf.float32)
+y_model = tf.matmul(x_data_, weights_)
 
-loss = tf.reduce_mean(tf.pow((y_model - y_data_),2))
-train_op = tf.train.GradientDescentOptimizer(0.01).minimize(loss)
+loss = tf.reduce_mean(tf.pow((y_model - y_data_), 2))
+
+# 学习速率的设置要多加小心
+train_op = tf.train.GradientDescentOptimizer(0.0001).minimize(loss)
 
 sess = tf.Session()
 init = tf.global_variables_initializer()
 sess.run(init)
 
-for _ in range(10):
-    for x,y in zip(x_data,y_data):
-        z1 = x.reshape(1,2)
-        z2 = y.reshape(1,1)
-        sess.run(train_op,feed_dict={x_data_:z1,y_data_:z2})
+for _ in range(30):
+    for x, y in zip(x_data, y_data):
+        z1 = x.reshape(1, 2)
+        z2 = y.reshape(1, 1)
+        sess.run(train_op, feed_dict={x_data_: z1, y_data_: z2})
+    print("weights = ")
     print(weights_.eval(sess))
+    print("loss = ")
+    print(sess.run(loss, feed_dict={x_data_: x_data, y_data_: y_data}))
+    print("------")
+tf.reset_default_graph()
