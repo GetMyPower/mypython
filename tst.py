fd0edfa4d9b2a72905fd47d1e71af4c1e81dc33f
GetMyPower <38427686+GetMyPower@users.noreply.github.com>
2019-11-03 11:01:14 +0800

    Update tst.py

--- tst.py
+++ tst.py
@@ -9,7 +9,6 @@ class MNISTLoader():
     def __init__(self):
         mnist = tf.keras.datasets.mnist
         (self.train_data, self.train_label), (self.test_data, self.test_label) = mnist.load_data()
-        # MNIST中的图像默认为uint8（0-255的数字）。以下代码将其归一化到0-1之间的浮点数，并在最后增加一维作为颜色通道
         self.train_data = np.expand_dims(self.train_data.astype(np.float32) / 255.0, axis=-1)      # [60000, 28, 28, 1]
         self.test_data = np.expand_dims(self.test_data.astype(np.float32) / 255.0, axis=-1)        # [10000, 28, 28, 1]
         self.train_label = self.train_label.astype(np.int32)    # [60000]
@@ -17,11 +16,10 @@ class MNISTLoader():
         self.num_train_data, self.num_test_data = self.train_data.shape[0], self.test_data.shape[0]
 
     def get_batch(self, batch_size):
-        # 从数据集中随机取出batch_size个元素并返回
         index = np.random.randint(0, np.shape(self.train_data)[0], batch_size)
         return self.train_data[index, :], self.train_label[index]
 
-# region
+
 class MLP(tf.keras.Model):
   def __init__(self):
     super().__init__()
@@ -35,7 +33,7 @@ class MLP(tf.keras.Model):
     output=tf.nn.softmax(x)
     return output
 
-# endregion
+
 
 num_epochs=5
 batch_size=50
@@ -55,7 +53,7 @@ for batch_index in range(num_batches):
     loss = tf.reduce_mean(loss)
 
     # print("batch %d: loss %f"%(batch_index,loss.numpy()))
-    if batch_index%100==0:
+    if batch_index%400==0:
       print("batch %d: loss %f"%(batch_index,loss.numpy()))
 
   grads=tape.gradient(loss,model.variables)
